7ce7db8a33a3df85d3439c99706e9f176b154240
jiangyong <jiang@hy12371.com>
2024-03-22 11:18:37 +0800

    monaco加载问题

--- apps/web/src/pages/templates/components/CustomCodeEditor.tsx
+++ apps/web/src/pages/templates/components/CustomCodeEditor.tsx
@@ -1,5 +1,5 @@
 import './CustomCodeEditor.css';
-import { Editor, Monaco } from '@monaco-editor/react';
+import { Editor, Monaco, loader } from '@monaco-editor/react';
 import { Card, Loader, useMantineColorScheme } from '@mantine/core';
 import { useCallback, useEffect, useRef } from 'react';
 import { colors } from '@novu/design-system';
@@ -8,7 +8,7 @@ import { editor as NEditor } from 'monaco-editor';
 import { createTranslationMarks } from './createTranslationMarks';
 import { IVariable, useWorkflowVariables } from '../../../api/hooks';
 import { useEnvController } from '@novu/shared-web';
-
+loader.config({ NEditor });
 export const CustomCodeEditor = ({
   onChange,
   value,
